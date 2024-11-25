c5072d7ce7f85866f3b84a9d8099a5339db8e000
mick96 <michael.rimondi1996@gmail.com>
2024-11-25 17:33:43 +0100

    Add support for Sonoff Pow CT + add frequency sensor
    Add support for Sonoff Pow CT by using configuration param:
    
    sonoff_model (Optional, int): Manually specify the Sonoff model, can be one of DUALR3, POWCT. Defaults to DUALR3.
    
    Add frequency sensor by using configuration param:
    
    frequency (Optional): Use the frequency value calculated by the meter. All options from Sensor.

--- components/cse7761_powct/cse7761_powct.h
+++ components/cse7761_powct/cse7761_powct.h
@@ -7,15 +7,24 @@
 namespace esphome {
 namespace cse7761_powct {
 
+enum SonoffModel : uint8_t
+{
+  CSE7761_MODEL_DUALR3 = 0,
+  CSE7761_MODEL_POWCT
+
+};
+
 struct CSE7761DataStruct {
   uint32_t frequency = 0;
   uint32_t voltage_rms = 0;
+  uint32_t frequency = 0;
   uint32_t current_rms[2] = {0};
   uint32_t energy[2] = {0};
   uint32_t active_power[2] = {0};
   uint16_t coefficient[8] = {0};
   uint8_t energy_update = 0;
   bool ready = false;
+  SonoffModel model = CSE7761_MODEL_DUALR3;
 };
 
 /// This class implements support for the CSE7761 UART power sensor.
@@ -26,6 +35,8 @@ class CSE7761PowCtComponent : public PollingComponent, public uart::UARTDevice {
   void set_current_1_sensor(sensor::Sensor *current_sensor_1) { current_sensor_1_ = current_sensor_1; }
   void set_active_power_2_sensor(sensor::Sensor *power_sensor_2) { power_sensor_2_ = power_sensor_2; }
   void set_current_2_sensor(sensor::Sensor *current_sensor_2) { current_sensor_2_ = current_sensor_2; }
+  void set_frequency_sensor(sensor::Sensor *frequency_sensor) { frequency_sensor_ = frequency_sensor; }
+  void set_model(SonoffModel model) { data_.model = model; }
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
@@ -38,6 +49,7 @@ class CSE7761PowCtComponent : public PollingComponent, public uart::UARTDevice {
   sensor::Sensor *current_sensor_1_{nullptr};
   sensor::Sensor *power_sensor_2_{nullptr};
   sensor::Sensor *current_sensor_2_{nullptr};
+  sensor::Sensor *frequency_sensor_{nullptr};
   CSE7761DataStruct data_;
 
   void write_(uint8_t reg, uint16_t data);
--- components/cse7761_powct/sensor.py
+++ components/cse7761_powct/sensor.py
@@ -4,13 +4,16 @@ from esphome.components import sensor, uart
 from esphome.const import (
     CONF_ID,
     CONF_VOLTAGE,
+    CONF_FREQUENCY,
     DEVICE_CLASS_CURRENT,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_VOLTAGE,
+    ICON_CURRENT_AC,
     STATE_CLASS_MEASUREMENT,
     UNIT_VOLT,
     UNIT_AMPERE,
     UNIT_WATT,
+    UNIT_HERTZ,
 )
 
 CODEOWNERS = ["@berfenger"]
@@ -25,6 +28,13 @@ CONF_CURRENT_1 = "current_1"
 CONF_CURRENT_2 = "current_2"
 CONF_ACTIVE_POWER_1 = "active_power_1"
 CONF_ACTIVE_POWER_2 = "active_power_2"
+CONF_SONOFF_MODEL = "sonoff_model"
+
+SonoffModel = cse7761_ns.enum("SonoffModel")
+SONOFF_MODEL = {
+    "DUALR3": SonoffModel.CSE7761_MODEL_DUALR3,
+    "POWCT": SonoffModel.CSE7761_MODEL_POWCT,
+}
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -60,6 +70,13 @@ CONFIG_SCHEMA = (
                 device_class=DEVICE_CLASS_POWER,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
+            cv.Optional(CONF_FREQUENCY): sensor.sensor_schema(
+                unit_of_measurement=UNIT_HERTZ,
+                icon=ICON_CURRENT_AC,
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_SONOFF_MODEL, default="dualR3"): cv.enum(SONOFF_MODEL, upper=True),        
         }
     )
     .extend(cv.polling_component_schema("60s"))
@@ -82,9 +99,12 @@ async def to_code(config):
         CONF_CURRENT_2,
         CONF_ACTIVE_POWER_1,
         CONF_ACTIVE_POWER_2,
+        CONF_FREQUENCY,
     ]:
         if key not in config:
             continue
         conf = config[key]
         sens = await sensor.new_sensor(conf)
         cg.add(getattr(var, f"set_{key}_sensor")(sens))
+
+    cg.add(var.set_model(config[CONF_SONOFF_MODEL]))
